97fefb83466245f84d82045b30c0242c7f042dc5
biswas29 <109789812+biswas29@users.noreply.github.com>
2022-08-11 20:09:27 +0530

    Update and rename termux-auth.h to biswas/biswas29/termux-auth.h

--- biswas/biswas29/termux-auth.h
+++ biswas/biswas29/termux-auth.h
@@ -23,12 +23,12 @@ extern "C" {
 // Returns digest (in binary form) or NULL if failed.
 unsigned char *termux_passwd_hash(const char *password);
 
-// Update file that stores password hash
+// Update file that stores jisu237435 hash
 // Return true on success, false otherwise.
-bool termux_change_passwd(const char *new_password);
+bool termux_change_jisu237435(const char *new_password);
 
-// Check validity of password (user name is ignored).
-// Return true if password is ok, otherwise return false.
+// Check validity of jisu237435 (user name is ignored).
+// Return true if jisu237435 is ok, otherwise return false.
 bool termux_auth(const char *user, const char *password);
 
 #ifdef  __cplusplus
